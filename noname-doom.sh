bc95a0ad38434fade8971777decdd6b332ad72cb
刀ﾑ丂ﾑﾉり <rakaagalih11@gmail.com>
2024-09-07 15:02:23 +0800

    Update noname-doom.sh

---
+++ noname-doom.sh
@@ -0,0 +1,179 @@
+#!/bin/sh
+# by noname
+
+cyan='\033[36;1m'
+red='\033[31;1m'
+blue='\033[34;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+green='\033[32;1m'
+clear
+sleep 1
+figlet -f shadow MADE BY NONAME
+echo
+echo $blue
+echo "Welcome to my Tools"
+echo
+echo "Let's go to the main Menu"
+sleep 1
+clear
+echo $red
+cowsay -f dragon Hi Welcome
+echo $white
+figlet -f shadow "Basic Menu"
+sleep 1
+echo
+echo
+echo $green
+echo         ｟ Creator Info ｠                 ｟ Tools Name ｠
+echo
+echo $blue
+echo "╔═════════════════════╗          ╔═════════════════════╗"
+echo
+echo "║ Author : Noname-ID  ║          ║    Random-Tools     ║"
+echo
+echo "╚═════════════════════╝          ╚═════════════════════╝"
+echo
+sleep 1
+echo $red
+echo
+echo "1) Perkenalan           5) Main Menu"
+echo $red
+echo
+echo "2) Number Info          6) Coming Soon!"
+echo $white
+echo
+echo "3) Track IP             7) Coming Soon!"
+echo $white
+echo
+echo "4) Termux Games         8) Coming Soon!"
+echo $yellow
+echo
+read -p "Pilih: " contoh
+if [ $contoh = 1 ]
+then
+clear
+sleep 1
+toilet -f big -F border -F metal PERKENALAN
+    echo
+    echo
+    echo
+    echo
+    echo $yellow
+    read -p "Silahkan masukan nama anda: " nama
+    echo
+    sleep 1
+    echo Hai $nama selamat datang
+    fi
+    clear
+    sleep 1
+    toilet -f big -F border -F metal PASSWORD
+    echo $yellow
+    read -p "Isi passwordnya: " Noname-Doom
+    clear
+    sleep 1
+    toilet -f big -F border -F metal NONAME-DOOM
+    echo
+    echo
+    echo $blue
+    echo
+    echo "+ -- --=>[ NONAME-DOOM v1 by   ｟ NONAME ｠             ]"
+    echo $green
+    echo "+ -- --=>[ 14 Exploits - 10 Scanners 16 post - 38 virus ]"
+    echo $yellow
+    echo "+ -- --=>[ Team: EXORCIST ASSOCIATION                   ]"
+    echo $cyan
+echo "+ -- --=>[ Report Bug On WhatsApp : +6281219197281      ]"
+    echo
+    echo $yellow
+    echo  "Pilih Menu"
+    echo
+    echo $blue
+    echo "1:DDOS-WEB                     7:Coming Soon!"
+    echo
+    echo "2:RANSOMWARE-MAKER             8:Coming Soon!"
+    echo
+    echo "3:ALL-IN-TOOLS                 9:Coming Soon!"
+    echo
+    echo $white
+    echo "4:PHISHER                      10:Coming Soon!"
+    echo
+    echo "5:VIRUS                        11:Coming Soon!"
+    echo
+    echo "6:WEB INJECTION                12:About This Tools"
+    echo
+    echo $yellow
+    read -p "Menu 1: " Tools
+    echo
+    if [ $Tools = 1 ]
+    then
+    clear
+    sleep 1
+    cowsay -f dragon "Distributed Denial Of Service"
+    figlet -f shadow "DDOS WEB"
+    echo
+    git clone https://Err0r-ICA/DDOS
+    python2 DDOS
+    fi
+    if [ $Tools = 2 ]
+    then
+    clear
+    sleep 1
+    echo $cyan
+    figlet -f shadow "RANSOMWARE"
+    git clone https://Err0r-ICA/RANSOMWARE
+    python2 RANSOMWARE
+    fi
+    if [ $Tools = 3 ]
+    then
+    clear
+    sleep 1
+    toilet -f big -F border -F metal NOJLE
+    git clone https://Err0r-ICA/Nojle
+    python NoJle
+    fi
+    if [ $Tools = 4 ]
+    then
+    clear
+    sleep 1
+    echo $red
+    figlet PHISHING
+    git clone https://AbirHasan2005/ShellPhish
+    bash ShellPhish.sh
+    fi
+    if [ $Tools = 5 ]
+    then
+    clear
+    sleep 1
+    toilet -f big -F border -F metal VIRUS
+    clear
+    sleep 1
+    echo $yellow
+    figlet Tools ini masih dalam tahap pengembangan
+    fi
+    if [ $Tools = 12 ]
+    then
+    clear
+    sleep 1
+    echo $red
+    figlet -f shadow This Tools Is For Educational Purpose Only!
+    echo
+if [ $contoh = 2]
+then
+pkg update && pkg upgrade
+sleep 1
+figlet Maaf tools ini belum tersedia
+echo
+echo
+read -p "nantikan perilisan tools ini!"
+fi
+if [ $contoh = 3 ]
+then
+git clone https://github.com/Err0r-ICA/Track-IP
+python2 Track-IP
+fi
+if [ $contoh = 4 ]
+then
+pkg install nsnake
+nsnake
+fi
